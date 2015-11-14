3e663a844193d542cd21b1b9818814c793fff03a
solteszjozsef <solteszjozsef@gmail.com>
2015-11-14 11:45:21 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,14 @@
+# Coursera Getting and Cleaning Data
 # 1. Merges the training and the test sets to create one data set.
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 # 3. Uses descriptive activity names to name the activities in the data set
 # 4. Appropriately labels the data set with descriptive variable names. 
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+loaddata1()
+loaddata2()
+loaddata3()
+exportdata()
 
 loaddata1 <- function(){
 
@@ -54,35 +60,4 @@ loaddata3<-function(){
 exportdata<-function(){
   write.table(tidydata,"tidydata.txt",row.names=FALSE)
   return("Tidy data exported")
-}
-loaddata666 <- function(){
-  
-testlabels<- read.table("UCI HAR Dataset/test/y_test.txt")
-names(testlabels)<-"Labels"
-labels<- read.table("UCI HAR Dataset/activity_labels.txt")
-testlabels<-data.frame(labels[testlabels$Labels,2])
-names(testlabels)<-"Labels"
-
-testfeatures<- read.table("UCI HAR Dataset/features.txt")
-
-
-colnames(testdata)<-testfeatures$V2
-testdata<-cbind(testlabels,testdata)
-
-
-
-
-trainlabels<- read.table("UCI HAR Dataset/train/y_train.txt")
-names(trainlabels)<-"Labels"
-labels<- read.table("UCI HAR Dataset/activity_labels.txt")
-trainlabels<-data.frame(labels[trainlabels$Labels,2])
-names(trainlabels)<-"Labels"
-
-trainfeatures<- read.table("UCI HAR Dataset/features.txt")
-
-colnames(traindata)<-trainfeatures$V2
-traindata<-cbind(trainlabels,traindata)
-
-fulldata<-rbind(testdata,traindata)
-return()
 }
